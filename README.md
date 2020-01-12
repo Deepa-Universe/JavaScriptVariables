8adca1ff11b363dfe346149f69f8da3632306c78
Deepa-Universe <deepa.miracles@gmail.com>
2020-01-12 23:10:15 +0530

    Variables Done

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # JavaScriptVariables
 
 This file has Let, Var, Const variables and understanding the difference of how they function.
+
+Difference- For 'var', if variable is used before initialization, it gives 'undefined', for 'let' and 'const' it will throw exception, cannot use variable before initialization.
+
+
+Important Note- Whether we use 'let','var','const'- Variable names must always be unique and not same.
+//Identifier 'xyz' has already been declared
---
+++ main.js
@@ -0,0 +1,36 @@
+//Variable
+
+//Let 
+//Define Variable
+
+//Exception throw
+//console.log(firstVariable); //Cannot access 'firstVariable' before initialization
+let firstVariable = 10;
+console.log(firstVariable); //10
+
+//Exception throw
+//let firstVariable = 20; //Identifier 'firstVariable' has already been declared
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Var
+//Define Variable
+
+//Exception throw 
+//var firstVariable = 20; //Identifier 'firstVariable' has already been declared, even with different key words same variable names cannot be used.
+
+console.log(intValue2); //For 'Var' if variable is called before initialization, it gives 'Undefined'
+var intValue2 = 20;
+console.log(intValue2); //20
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Const
+//Define Variable
+
+//Exception throw
+//console.log(intValue3); //Cannot access 'intValue3' before initialization
+//Exception throw
+//const intValue2 = 40; //Identifier 'intValue2' has already been declared
+const intValue3 = 30;
+console.log(intValue3);
